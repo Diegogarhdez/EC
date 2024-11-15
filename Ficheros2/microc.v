baa0ae8144b5d9f24f2da882911fa62b1e7783e0
usuario <alu0101633732@ull.edu.es>
2024-11-15 18:54:05 +0000

    cambios en microc.v (usa git pull por si acaso)

--- Ficheros2/microc.v
+++ Ficheros2/microc.v
@@ -4,6 +4,7 @@ module microc(output wire [5:0] Opcode, output wire z, input wire clk, reset, s_
   wire[9:0] PC_actual,PC_nuevo;
   wire [15:0] Instruccion;  
 
+  registro #(10) registro_pc(PC_actual, clk, reset, PC_nuevo);
   memprog memoria_programa(Instruccion,clk,PC_actual);
 
   assign Opcode = Instruccion[15:10];
@@ -19,25 +20,12 @@ module microc(output wire [5:0] Opcode, output wire z, input wire clk, reset, s_
   wire [7:0] RD1, RD2, WD3, Inmediato;
   assign Inmediato = Instruccion[11:4];
 
-  mux2_8 mux_alu(WD3, Inmediato, salida_alu, s_inm);
-  sum1 suma1(PC_nuevo, PC_nuevo);
-  mux2_10 mux_pc(PC_nuevo, Dir_salto, PC_actual, s_inc);
+  mux2 #(8) mux_alu(WD3, Inmediato, salida_alu, s_inm);
+  sum suma1(PC_nuevo, PC_nuevo, 10'b0000000001);
+  mux2 #(10) mux_pc(PC_nuevo, Dir_salto, PC_actual, s_inc);
 
   regfile bankanshat(RD1, RD2, clk, we3, RA1, RA2, WA3, WD3);
   alu alu(salida_alu, zero, RD1, RD2, Op);
   ffd ffd1(clk, reset, zero, wez, zero);
 
-endmodule
-
-
-module mux2_10(output wire [9:0] PC_nuevo, input wire[9:0] Dir_salto, PC_actual, input wire s_inc);
-  assign PC_nuevo = s_inc? PC_actual : Dir_salto;
-endmodule
-
-module mux2_8(output wire [7:0] WD3, input wire [7:0] Inmediato, salida_alu, input wire s_inm);
-  assign WD3 = s_inm? Inmediato : salida_alu;
-endmodule
-
-module sum1(output wire [9:0] PC_mas_1, input wire [9:0] PC_actual);
-  assign PC_mas_1 = PC_actual + 10'b0000000001;
 endmodule
