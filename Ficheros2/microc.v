b8e763896afbc54614e852ebaa310f3cae7bdb8a
Diegogarhdez <145361345+Diegogarhdez@users.noreply.github.com>
2024-11-20 16:53:09 +0000

    Update microc.v

--- Ficheros2/microc.v
+++ Ficheros2/microc.v
@@ -7,7 +7,7 @@ module microc(
   // Declaración de variables internas
   wire [9:0] PC_actual, PC_nuevo, PC_incrementado;
   wire [15:0] Instruccion;
-  wire [7:0] RD1, RD2, WD3, Inmediato, salida_alu;
+  wire [7:0] RD1, RD2, WD3, Inmediato;
   wire [3:0] RA1, RA2, WA3;
   wire zero;
   wire [9:0] Dir_salto;
@@ -29,7 +29,7 @@ module microc(
   assign WA3 = Instruccion[3:0];
 
   // Valor inmediato
-  assign Inmediato = Instruccion[7:0];  // Ajustado a 8 bits
+  assign Inmediato = Instruccion[11:4];  // Ajustado a 8 bits
 
   // Sumador para PC
   sum sum_pc(PC_incrementado, PC_actual, 10'b0000000001);
@@ -37,16 +37,22 @@ module microc(
   // Mux para actualizar PC
   mux2 #(10) mux_pc(PC_nuevo, Dir_salto, PC_incrementado, s_inc);
 
+  // Mux para entrada al banco de registros
+  wire[3:0] salida_muxreg;
+  mux2 #(4) mux_reg(salida_muxreg, RA2, WA3, s_inm);   
+
   // Banco de registros
-  regfile banco_registros(RD1, RD2, clk, we3, RA1, RA2, WA3, WD3);
+  regfile banco_registros(RD1, RD2, clk, we3, RA1, salida_muxreg, WA3, WD3);
+
+  // Mux para la entrada a la alu
+  wire[7:0] salida_muxalu; 
+  mux2 #(8) mux_alu(salida_muxalu, RD1, Inmediato, s_inm); 
 
   // ALU
-  alu alu_inst(salida_alu, zero, RD1, RD2, Op);
-
-  // Mux para seleccionar entre salida de ALU e inmediato
-  mux2 #(8) mux_alu(WD3, salida_alu, Inmediato, s_inm);
+  alu alu_inst(WD3, zero, salida_muxalu, RD2, Op);
 
   // Flip-Flop Tipo D
   ffd registro_zero(clk, reset, zero, wez, z);
 
 endmodule
+
